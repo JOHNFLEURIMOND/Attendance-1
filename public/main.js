d8660cc4d2257448a372d686ebe83d44b82762c5
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2018-03-12 13:31:32 -0400

    edits

--- public/main.js
+++ public/main.js
@@ -1,64 +1,64 @@
-alert("Miracles happen all the time, We're here aren't we ? -Marilyn Nelson");
-
-var present = []
-
-var tardy = []
-
-$('#all').click(function() {
-
-  $('.present-list > li, .tardy-list > li').toggle();
-
-});
-
-function presentList() {
-
-  var value = $("#enter").val()
-
-  present.push(value);
-
-  $(".present-list li").remove();
-
-  present.forEach(function(e) {
-
-    $(".present-list").append("<li>" + e + "</li>");
-
-    document.getElementById('e').value = "";
-
-  })
-
-}
-
-
-
-function tardyList() {
-
-  var values = $("#enter").val()
-
-  tardy.push(values);
-
-  $(".tardy-list li").remove();
-
-  tardy.forEach(function(e) {
-
-    $(".tardy-list").append("<li>" + e + "</li>");
-
-    document.getElementById('enter').value = "";
-
-  })
-
-}
-
-
-
-$(document).ready(function() {
-
-  $("#present").on("click", presentList);
-
-  $("#tardy").on("click", tardyList);
-
-  $("#all").on("click", allList);
-
-});
+// alert("Miracles happen all the time, We're here aren't we ? -Marilyn Nelson");
+//
+// var present = []
+//
+// var tardy = []
+//
+// $('#all').click(function() {
+//
+//   $('.present-list > li, .tardy-list > li').toggle();
+//
+// });
+//
+// function presentList() {
+//
+//   var value = $("#enter").val()
+//
+//   present.push(value);
+//
+//   $(".present-list li").remove();
+//
+//   present.forEach(function(e) {
+//
+//     $(".present-list").append("<li>" + e + "</li>");
+//
+//     document.getElementById('e').value = "";
+//
+//   })
+//
+// }
+//
+//
+//
+// function tardyList() {
+//
+//   var values = $("#enter").val()
+//
+//   tardy.push(values);
+//
+//   $(".tardy-list li").remove();
+//
+//   tardy.forEach(function(e) {
+//
+//     $(".tardy-list").append("<li>" + e + "</li>");
+//
+//     document.getElementById('enter').value = "";
+//
+//   })
+//
+// }
+//
+//
+//
+// $(document).ready(function() {
+//
+//   $("#present").on("click", presentList);
+//
+//   $("#tardy").on("click", tardyList);
+//
+//   $("#all").on("click", allList);
+//
+// });
 
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
 var thumbDown = document.getElementsByClassName("fa-thumbs-down");
