8f24f9043046e2800fc0cb71cbe9273c2b6c6c73
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2018-03-13 11:06:35 -0400

    edits

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
+  db.collection('attendance').find().toArray((err, result) => {
     if (err) return console.log(err)
     res.render('index.ejs', {
       messages: result
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-  db.collection('messages').save({
+  db.collection('attendance').save({
     name: req.body.name,
     msg: req.body.msg,
     thumbUp: 0,
@@ -42,48 +42,9 @@ app.post('/messages', (req, res) => {
   })
 })
 
-app.put('/thumbUp', (req, res) => {
-  db.collection('messages')
-    .findOneAndUpdate({
-      name: req.body.name,
-      msg: req.body.msg
-    }, {
-      $set: {
-        thumbUp: req.body.thumbUp + 1
-      }
-    }, {
-      sort: {
-        _id: -1
-      },
-      upsert: true
-    }, (err, result) => {
-      if (err) return res.send(err)
-      res.send(result)
-    })
-})
-
-app.put('/thumbDown', (req, res) => {
-  db.collection('messages')
-    .findOneAndUpdate({
-      name: req.body.name,
-      msg: req.body.msg
-    }, {
-      $set: {
-        thumbDown: req.body.thumbDown + 1
-      }
-    }, {
-      sort: {
-        _id: -1
-      },
-      upsert: true
-    }, (err, result) => {
-      if (err) return res.send(err)
-      res.send(result)
-    })
-})
 
 app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({
+  db.collection('attendance').findOneAndDelete({
     name: req.body.name,
     msg: req.body.msg
   }, (err, result) => {
